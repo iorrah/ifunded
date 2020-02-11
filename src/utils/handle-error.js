bc321f01d4a1cfdc47d6631cdd3eb370ad7ead9f
Iorrah Mota <iorrah.gonzalez@gmail.com>
2020-02-11 18:08:24 +0100

    Prevent console.log crash

--- src/utils/handle-error.js
+++ src/utils/handle-error.js
@@ -1,10 +1,12 @@
 const handleInternalError = (error) => {
   const string = `Internal Error (${error}): the client could not complete the transaction`;
+  // eslint-disable-next-line no-console
   console.error(string);
 };
 
 const handleServerError = (error) => {
   const string = `Server Error ${error}: the REST API could not complete the transaction`;
+  // eslint-disable-next-line no-console
   console.error(string);
 };
 
