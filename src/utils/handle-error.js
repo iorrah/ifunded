c55b07256073214b6b6aad32abacc4bb58ea669e
Iorrah Mota <iorrah.gonzalez@gmail.com>
2020-02-11 15:59:58 +0100

    Make error messages more generic

--- src/utils/handle-error.js
+++ src/utils/handle-error.js
@@ -1,10 +1,10 @@
 const handleInternalError = (error) => {
-  const string = `Internal Error (${error}): the client could not create the new user`;
+  const string = `Internal Error (${error}): the client could not complete the transaction`;
   console.error(string);
 };
 
 const handleServerError = (error) => {
-  const string = `Server Error ${error}: the REST API could not create the new user`;
+  const string = `Server Error ${error}: the REST API could not complete the transaction`;
   console.error(string);
 };
 
