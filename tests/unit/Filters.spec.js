51cd64ca0248a81ad73c9596e00c49e3666cbbe5
Iorrah Mota <iorrah.gonzalez@gmail.com>
2020-02-12 17:39:53 +0100

    Test the Filters

---
+++ tests/unit/Filters.spec.js
@@ -0,0 +1,35 @@
+import { mount } from '@vue/test-utils';
+import Filters from '../../src/components/Filters/index';
+
+describe('Filters', () => {
+  test('renders correctly', () => {
+    const props = {
+      propsData: {
+        appUsers: [
+          {
+            id: 1,
+            first_name: 'Jane',
+            last_name: 'Doe',
+            email: 'jane.doeemail.com',
+            created_date: new Date('2019-02-12'), // Feb
+            creator_id: 1,
+          },
+          {
+            id: 2,
+            first_name: 'Bob',
+            last_name: 'Aly',
+            email: 'bob.aly@email.com',
+            created_date: new Date('2019-12-02'), // Dec
+            creator_id: 1,
+          },
+        ],
+        creatorId: 1,
+        month: 2,
+        search: 'Doe',
+      },
+    };
+
+    const wrapper = mount(Filters, props);
+    expect(wrapper.element).toMatchSnapshot();
+  });
+});
