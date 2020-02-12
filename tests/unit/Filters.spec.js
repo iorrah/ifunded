d9a6c25b4178f658d22f126702eaab3723f4edff
Iorrah Mota <iorrah.gonzalez@gmail.com>
2020-02-12 17:41:32 +0100

    Move params outside of assertation

--- tests/unit/Filters.spec.js
+++ tests/unit/Filters.spec.js
@@ -1,34 +1,34 @@
 import { mount } from '@vue/test-utils';
 import Filters from '../../src/components/Filters/index';
 
+const props = {
+  propsData: {
+    appUsers: [
+      {
+        id: 1,
+        first_name: 'Jane',
+        last_name: 'Doe',
+        email: 'jane.doeemail.com',
+        created_date: new Date('2019-02-12'), // Feb
+        creator_id: 1,
+      },
+      {
+        id: 2,
+        first_name: 'Bob',
+        last_name: 'Aly',
+        email: 'bob.aly@email.com',
+        created_date: new Date('2019-12-02'), // Dec
+        creator_id: 1,
+      },
+    ],
+    creatorId: 1,
+    month: 2,
+    search: 'Doe',
+  },
+};
+
 describe('Filters', () => {
   test('renders correctly', () => {
-    const props = {
-      propsData: {
-        appUsers: [
-          {
-            id: 1,
-            first_name: 'Jane',
-            last_name: 'Doe',
-            email: 'jane.doeemail.com',
-            created_date: new Date('2019-02-12'), // Feb
-            creator_id: 1,
-          },
-          {
-            id: 2,
-            first_name: 'Bob',
-            last_name: 'Aly',
-            email: 'bob.aly@email.com',
-            created_date: new Date('2019-12-02'), // Dec
-            creator_id: 1,
-          },
-        ],
-        creatorId: 1,
-        month: 2,
-        search: 'Doe',
-      },
-    };
-
     const wrapper = mount(Filters, props);
     expect(wrapper.element).toMatchSnapshot();
   });
