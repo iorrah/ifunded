0a57f222ffdcdeaf07c84b711a32af33bd4a258e
Iorrah Mota <iorrah.gonzalez@gmail.com>
2020-02-12 17:46:36 +0100

    Test the Edit User Modal

---
+++ tests/unit/EditModal.spec.js
@@ -0,0 +1,24 @@
+import { mount } from '@vue/test-utils';
+import EditModal from '../../src/components/EditModal/index';
+
+const props = {
+  propsData: {
+    dispatschEditUser: jest.fn(),
+    closeEditModal: jest.fn(),
+    user: {
+      id: 1,
+      first_name: 'Jane',
+      last_name: 'Doe',
+      email: 'jane.doeemail.com',
+      created_date: new Date('2019-02-12'), // Feb
+      creator_id: 1,
+    },
+  },
+};
+
+describe('EditModal', () => {
+  test('renders correctly', () => {
+    const wrapper = mount(EditModal, props);
+    expect(wrapper.element).toMatchSnapshot();
+  });
+});
